8e21dd067e2e7861334cf35efc3620f7cd19fa49
KosmasProjects <kosma.cieliczko@wp.pl>
2026-06-20 15:33:32 +0200

    test 4

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Testowa strona 123456"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
