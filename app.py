7402fa8a6ffcbaa3dd0d16d43116d71c36cb5c91
KosmasProjects <kosma.cieliczko@wp.pl>
2026-06-20 15:29:21 +0200

    test 4

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "VERSION 3 - AUTO DEPLOY"
+    return "Testowa strona 123456"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
