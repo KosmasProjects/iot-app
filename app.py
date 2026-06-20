db2061b36116e5d52d76c0ab01a3c8bd614d74a0
KosmasProjects <kosma.cieliczko@wp.pl>
2026-06-20 15:16:36 +0200

    test cd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "OK AZURE"
+    return "VERSION 2 - AUTO DEPLOY"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
